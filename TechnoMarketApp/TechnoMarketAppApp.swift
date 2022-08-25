22a1011673096c007fbbe0d7bb45e3a951448256
Rashid Gaitov <rashidgaitov@gmail.com>
2022-08-25 13:17:04 +0300

    Initial Commit

---
+++ TechnoMarketApp/TechnoMarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnoMarketAppApp.swift
+//  TechnoMarketApp
+//
+//  Created by Rashid Gaitov on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TechnoMarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
